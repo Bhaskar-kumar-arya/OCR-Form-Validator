32f11906e359807f799efd9b12ac7c035acf1885
devestrator <bhaskarkumararya900@gmail.com>
2025-09-03 21:52:04 +0530

    removed unnecesary files

--- api/example.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# Downloads the full model, including processor configs
-local_path = snapshot_download(repo_id="microsoft/trocr-base-printed", cache_dir="./trocr_model")
-print("Model downloaded to:", local_path)
